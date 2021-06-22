97289c793af0e96c15c2f5e9c735181aee4d0c63
Victor Queiroz <victoorqrz@gmail.com>
2021-06-21 23:11:06 -0300

    creating userRepository and CreateUserService

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,35 @@
+import { UserRepositories } from "../repositories/UsersRepositories";
+
+interface IUserRequest {
+  name: string;
+  email: string;
+  admin?: boolean;
+}
+
+class CreateUserService {
+  async execute({ name, email, admin }: IUserRequest) {
+    const userRepository = new UserRepositories();
+
+    if (!email) {
+      throw new Error("Email incorrect");
+    }
+
+    const userAlreadyExists = await userRepository.findOne();
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const user = userRepository.create({
+      name,
+      email,
+      admin,
+    });
+
+    await userRepository.save(user);
+
+    return user;
+  }
+}
+
+export { CreateUserService };
